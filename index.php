87fdfecd665a11291a40ab343a7d5ad4eb7c94ee
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-05-22 12:29:32 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Template Name: Index
+*/
+
+require_once 'functions.php';
+
+get_header(); ?>
+
+<div id="content">
+  <h2 class="header-center">WHY SHOULD YOU LISTEN TO ME?</h2>
+  <p class="header-center">I help women achieve their goals with simple, empowering strength training programs.</p>
+  <hr>
+  <div class="wp-container">
+    <div id="primary" class="site-content row">
+      <div id="content" role="main" class="col-sm-12 col-md-6 col-md-offset-3">
+          <div>
+            <span class="dashicons dashicons-external img-center"></span>
+            <h4 class="header-center">NO NONSENSE</h4>
+            <p class="header-center">No gimmicks, quick-fix fads, or "fix your flaws and finally love yourself" BS.</p>
+          </div>
+          <hr>
+          <div>
+            <span class="dashicons dashicons-portfolio img-center"></span>
+            <h4 class="header-center">PROVEN EXPERIENCE</h4>
+            <p class="header-center">I've worked with clients for over a decade, and I know what works.</p>
+          </div>
+          <hr>
+          <div>
+            <span class="dashicons dashicons-universal-access-alt img-center"></span>
+            <h4 class="header-center">SANE AND SIMPLE</h4>
+            <p class="header-center">No deprivation, exhaustion, or obsessive eating habits required.</p>
+          </div>
+          <hr>
+          <div>
+            <span class="dashicons dashicons-chart-bar img-center"></span>
+            <h4 class="header-center">EMPOWERMENT</h4>
+            <p class="header-center">Health and fitness information that makes you feel great about yourself.</p>
+          </div>
+          <hr>
+          <div>
+            <span class="dashicons dashicons-nametag img-center"></span>
+            <h4 class="header-center">SUSTAINABLE</h4>
+            <p class="header-center">Workout and nutrition information that fits your life, and doesn't dominate it.</p>
+          </div>
+          <hr>
+          <div>
+            <span class="dashicons dashicons-calendar-alt img-center"></span>
+            <h4 class="header-center">UNLEASH YOUR AWESOME</h4>
+            <p class="header-center">Stop trying to exercise regularly. Harness the power of training with purpose.</p>
+          </div>
+          <hr>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
